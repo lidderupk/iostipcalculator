b51cb846e07d09874f2e4feea6bc9d14d52e3890
Upkar Lidder <ulidder@Shiraz.local>
2013-12-08 12:14:05 -0800

    first commit

---
+++ TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Upkar Lidder on 2013-12-08.
+//  Copyright (c) 2013 upkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Upkar Lidder on 2013-12-08.
+//  Copyright (c) 2013 upkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
