7585a70a3f148a0332e6b6311049b884cc496284
Upkar Lidder <ulidder@Shiraz.local>
2013-12-08 13:59:29 -0800

    finish tip calculator

--- TipViewController.m
+++ TipViewController.m
@@ -9,7 +9,12 @@
 #import "TipViewController.h"
 
 @interface TipViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+- (IBAction)onTap:(id)sender;
+- (void)updateValues;
 @end
 
 @implementation TipViewController
@@ -19,6 +24,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        self.title = @"Tip Calculator";
     }
     return self;
 }
@@ -27,6 +33,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self updateValues];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +42,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+    [self updateValues];
+}
+
+- (void) updateValues{
+    float billAmount = [self.billTextField.text floatValue];
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.20)];
+    float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+    float totalAmount = tipAmount + billAmount;
+    
+    //write back to the labels
+    self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+}
 @end
